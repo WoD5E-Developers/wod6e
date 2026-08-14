25ad7e306838d771120a78f1d0056799094db591
Veilza <113429647+Veilza@users.noreply.github.com>
2026-08-14 13:11:57 -0500

    Fix issue with localization of willpower cost on disciplines

--- system/splats/vampire/actors/actor-sheets/scripts/prepare-vampire-partials.js
+++ system/splats/vampire/actors/actor-sheets/scripts/prepare-vampire-partials.js
@@ -202,7 +202,8 @@ export function prepareDisciplinesContext(context, actor) {
               ? game.i18n.localize('WOD6E.None')
               : game.i18n.format('WOD6E.ITEMS.NumberStringResourceCost', {
                   number: item.system?.activation?.cost?.amount,
-                  string: game.i18n.localize(context[item.system?.activation?.cost?.type]?.label)
+                  string: WOD6E.configs.CostTypes.getList({})[item.system?.activation?.cost?.type]
+                    .label
                 })
         }))
 
