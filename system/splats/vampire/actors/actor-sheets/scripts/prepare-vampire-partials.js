9146360bb3c00d64b7651a69aa147345277400fe
Veilza <113429647+Veilza@users.noreply.github.com>
2026-08-22 14:32:03 -0500

    Allow for drag-and-drop sorting of items

--- system/splats/vampire/actors/actor-sheets/scripts/prepare-vampire-partials.js
+++ system/splats/vampire/actors/actor-sheets/scripts/prepare-vampire-partials.js
@@ -77,9 +77,12 @@ export const prepareLeftColumnContext = async function (context, actor) {
 }
 
 export const prepareMiddleColumnContext = async function (context, actor) {
-  const equipment = actor.items.filter((item) => item.type === 'equipment')
+  const equipment = actor.items
+    .filter((item) => item.type === 'equipment')
+    .sort((a, b) => a.sort - b.sort)
   context.equipment = await Promise.all(
     equipment.map(async (item) => ({
+      id: item.id,
       uuid: item.uuid,
       name: item.name,
       img: item.img,
@@ -93,9 +96,12 @@ export const prepareMiddleColumnContext = async function (context, actor) {
 }
 
 export const prepareRightColumnContext = async function (context, actor) {
-  const lifepaths = actor.items.filter((item) => item.type === 'lifepath')
+  const lifepaths = actor.items
+    .filter((item) => item.type === 'lifepath')
+    .sort((a, b) => a.sort - b.sort)
   context.lifepaths = await Promise.all(
     lifepaths.map(async (lifepath) => ({
+      id: lifepath.id,
       uuid: lifepath.uuid,
       name: lifepath.name,
       img: lifepath.img,
@@ -105,9 +111,12 @@ export const prepareRightColumnContext = async function (context, actor) {
     }))
   )
 
-  const clanTraits = actor.items.filter((item) => item.type === 'clanTrait')
+  const clanTraits = actor.items
+    .filter((item) => item.type === 'clanTrait')
+    .sort((a, b) => a.sort - b.sort)
   context.clanTraits = await Promise.all(
     clanTraits.map(async (clanTrait) => ({
+      id: clanTrait.id,
       uuid: clanTrait.uuid,
       name: clanTrait.name,
       img: clanTrait.img,
@@ -117,9 +126,10 @@ export const prepareRightColumnContext = async function (context, actor) {
     }))
   )
 
-  const merits = actor.items.filter((item) => item.type === 'merit')
+  const merits = actor.items.filter((item) => item.type === 'merit').sort((a, b) => a.sort - b.sort)
   context.merits = await Promise.all(
     merits.map(async (merit) => ({
+      id: merit.id,
       uuid: merit.uuid,
       name: merit.name,
       img: merit.img,
@@ -129,9 +139,10 @@ export const prepareRightColumnContext = async function (context, actor) {
     }))
   )
 
-  const flaws = actor.items.filter((item) => item.type === 'flaw')
+  const flaws = actor.items.filter((item) => item.type === 'flaw').sort((a, b) => a.sort - b.sort)
   context.flaws = await Promise.all(
     flaws.map(async (flaw) => ({
+      id: flaw.id,
       uuid: flaw.uuid,
       name: flaw.name,
       img: flaw.img,
@@ -211,8 +222,10 @@ export function prepareDisciplinesContext(context, actor) {
       const max = actorDiscipline?.max ?? 5
       const powers = actor.items
         .filter((item) => item.type === 'discipline' && item.system?.disciplineType === key)
+        .sort((a, b) => a.sort - b.sort)
         .map((item) => ({
           ...item,
+          id: item.id,
           uuid: item.uuid,
           testText: generateTestTextFromItem(item),
           cost:
